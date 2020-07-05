97359790483de9dde62481559f4527b499bdee17
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-07-05 13:56:33 +0300

    Adding DbContext

--- IdentityExample/Startup.cs
+++ IdentityExample/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityExample.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -21,6 +24,11 @@ namespace IdentityExample
             //         options.LoginPath = "/Home/Authenticate";
 
             //     });
+            services.AddDbContext<AppDbContext>(config =>
+            {
+                config.UseInMemoryDatabase("Memory");
+            });
+            services.AddIdentity<IdentityUser, IdentityRole>();
             services.AddControllersWithViews();
         }
 
